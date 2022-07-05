8c027537e3aea809f69b99bde0728d73758fede7
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-07-05 16:30:47 -0400

    env file added, lots more work done

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 2 }}
-        classname='app_header-info'
+        className='app_header-info'
       >
         <div className='app_header-badge'>
           <div className='badge-cmp app__flex'>
@@ -39,13 +39,12 @@ const Header = () => {
       <motion.div
         whileInView={{ opacity: [0, 1] }}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
-        classname='app_header-img'
+        className='app_header-img'
       >
         <img src={images.profile} alt='profile_bg' />
         <motion.img
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: "easeInOut" }}
-          classname='app_header-img'
           src={images.circle}
           className='overlay_circle'
         ></motion.img>
