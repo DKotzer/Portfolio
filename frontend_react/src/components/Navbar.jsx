cfd4775b38ba32468c91326a68d1040c8b351303
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-07-05 14:27:03 -0400

    navbar fixed

--- frontend_react/src/components/Navbar.jsx
+++ frontend_react/src/components/Navbar.jsx
@@ -12,24 +12,25 @@ const Navbar = () => {
       </div>
       <ul className='app__navbar-links'>
         {["home", "about", "work", "skills", "contact"].map((item) => (
-          <li className='app_flex p-text' key={`link-${item}`}>
+          <li className='app__flex p-text' key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
+
       <div className='app__navbar-menu'>
         <HiMenuAlt4 onClick={() => setToggle(true)} />
+
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.55, ease: "easeOut" }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
               {["home", "about", "work", "skills", "contact"].map((item) => (
                 <li key={item}>
-                  <div />
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
                   </a>
